bad99d626eab844503dd048d7377a328ad532d6b
marting98 <103552411+marting98@users.noreply.github.com>
2022-05-21 21:34:39 -0300

    Add files via upload
    agrego mas assert

--- TRABAJO 01/tp 1 assert .cpp
+++ TRABAJO 01/tp 1 assert .cpp
@@ -1,30 +1,40 @@
 #include<string>
 #include<cassert>
+using namespace std::string_literals;
 int main() {
     //bool
+    assert(true);
    assert(false== not true);
    assert((true and true)==true);
    assert((true or false)==true);
    assert((false or false)==not true);
 
    //double
-    assert(1.4<1.8);
-    assert(4.5!=42.3);
-    assert(2.6==1.3+1.3);
+    assert(1.42<1.82);
+    assert(4.53!=42.34);
+    assert(2.62==1.31+1.31);
+    assert(4.24-2.14==2.10);
+    assert(4.50/1.50==3.0);
+    assert(32.44>23.56);
   // unssigned
-   assert(2==1*2);
-   assert(8>2);
-   assert(321!=3*43);
-   assert(24/2==12);
-   assert(3<7);
+   assert(2u==1u*2u);
+   assert(8u>2u);
+   assert(321u!=3u*43u);
+   assert(24u/2u==12);
+   assert(3u<7u);
    //char
 
    assert('a'!='d');
    assert(65=='A');
    assert(65+32=='a');
    assert(65+1=='B');
+   assert('d'+'q'==0xD5);
+   assert('C'-'A'==2);
+   assert('C'>'A');
+   assert('A'<'a');
+
 //string
    assert(10==std::string("hola q tal").length());
-
+   assert(12=="hola a todos"s.length());
 
 }
